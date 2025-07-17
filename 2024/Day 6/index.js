d1a6066abecad6b8d3a58ba84d81060c8dfcbcde
Josh Lawrence <josh@joshlawrence.dev>
2025-07-16 20:32:08 -0400

    feat(2024): completed day 6 part 2

--- 2024/Day 6/index.js
+++ 2024/Day 6/index.js
@@ -5,10 +5,14 @@ export class Guard {
   constructor(character, row, column) {
     this.heading = null;
     this.setHeading(character);
+    this.initalHeading = this.heading;
     this.row = row;
+    this.initalPosition = [row, column];
     this.column = column;
-    this.visitedPositions = new Map([[`${row},${column}`, true]]);
-    this.counter = 0;
+    this.visitedPositions = new Map([
+      [`${row},${column}${this.heading}`, true],
+    ]);
+    this.visitedPositionsNoHeading = new Map([[`${row},${column}`, true]]);
   }
 
   getPosition() {
@@ -17,6 +21,10 @@ export class Guard {
 
   getVisitedPositions() {
     return this.visitedPositions.size;
+  }
+
+  getVisitedPositionsNoHeading() {
+    return this.visitedPositionsNoHeading.size;
   }
 
   setHeading(character) {
@@ -59,24 +67,67 @@ export class Guard {
     switch (this.heading) {
       case "NORTH":
         this.row -= 1;
-        this.visitedPositions.set(`${this.row},${this.column}`, true);
-        this.counter++;
+        this.visitedPositions.set(
+          `${this.row},${this.column}${this.heading}`,
+          true,
+        );
+        this.visitedPositionsNoHeading.set(`${this.row},${this.column}`, true);
         break;
       case "EAST":
         this.column += 1;
-        this.visitedPositions.set(`${this.row},${this.column}`, true);
-        this.counter++;
+        this.visitedPositions.set(
+          `${this.row},${this.column}${this.heading}`,
+          true,
+        );
+        this.visitedPositionsNoHeading.set(`${this.row},${this.column}`, true);
         break;
       case "SOUTH":
         this.row += 1;
-        this.visitedPositions.set(`${this.row},${this.column}`, true);
-        this.counter++;
+        this.visitedPositions.set(
+          `${this.row},${this.column}${this.heading}`,
+          true,
+        );
+        this.visitedPositionsNoHeading.set(`${this.row},${this.column}`, true);
         break;
       case "WEST":
         this.column -= 1;
-        this.visitedPositions.set(`${this.row},${this.column}`, true);
-        this.counter++;
+        this.visitedPositions.set(
+          `${this.row},${this.column}${this.heading}`,
+          true,
+        );
+        this.visitedPositionsNoHeading.set(`${this.row},${this.column}`, true);
         break;
+    }
+  }
+
+  resetPosition() {
+    this.row = this.initalPosition[0];
+    this.column = this.initalPosition[1];
+    this.heading = this.initalHeading;
+    this.visitedPositions.clear();
+  }
+
+  isNewPosition(rowsToMove, columnsToMove, grid) {
+    if (
+      this.visitedPositions.has(
+        `${this.row + rowsToMove},${this.column + columnsToMove}${this.heading}`,
+      )
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  canMove(rowsToMove, columnsToMove, grid) {
+    if (
+      typeof grid[this.row + rowsToMove] === "undefined" ||
+      typeof grid[this.row + rowsToMove][this.column + columnsToMove] ===
+        "undefined"
+    ) {
+      return false;
+    } else {
+      return true;
     }
   }
 }
@@ -96,6 +147,8 @@ export class Simulator {
   }
 
   run() {
+    let result = true;
+
     while (true) {
       let rowMod = 0;
       let columnMod = 0;
@@ -115,36 +168,89 @@ export class Simulator {
           break;
       }
 
-      if (
-        typeof this.grid[this.guard.row + rowMod] === "undefined" ||
-        typeof this.grid[this.guard.row + rowMod][
-          this.guard.column + columnMod
-        ] === "undefined"
-      ) {
+      if (!this.guard.canMove(rowMod, columnMod, this.grid)) {
+        result = true;
         break;
-      } else if (
+      }
+
+      if (!this.guard.isNewPosition(rowMod, columnMod, this.grid)) {
+        result = false; // return false if guard loops
+        break;
+      }
+
+      if (
         this.grid[this.guard.row + rowMod][this.guard.column + columnMod] ===
         "#"
+      ) {
+        this.guard.collide();
+      } else if (
+        this.grid[this.guard.row + rowMod][this.guard.column + columnMod] ===
+        "O"
       ) {
         this.guard.collide();
       } else {
         this.guard.move();
       }
     }
+
+    return result;
   }
 }
 
 export function solvePart1(simulator) {
   simulator.run();
-  return simulator.guard.getVisitedPositions();
+  return simulator.guard.getVisitedPositionsNoHeading();
+}
+
+export function solvePart2(simulator) {
+  let obstaclePositions = 0;
+
+  for (let row = 0; row < simulator.grid.length; row++) {
+    for (let column = 0; column < simulator.grid[0].length; column++) {
+      if (
+        simulator.guard.initalPosition[0] === row &&
+        simulator.guard.initalPosition[1] === column
+      ) {
+        continue;
+      }
+
+      if (simulator.grid[row][column] === "#") {
+        continue;
+      }
+
+      let originalRow = simulator.grid[row];
+
+      let tempRow =
+        simulator.grid[row].slice(0, column) +
+        "O" +
+        simulator.grid[row].slice(column + 1, simulator.grid[row].length);
+
+      simulator.grid[row] = tempRow;
+      let result = simulator.run(true, row, column);
+      simulator.grid[row] = originalRow;
+
+      simulator.guard.resetPosition();
+
+      if (!result) {
+        obstaclePositions++;
+      }
+    }
+  }
+
+  return obstaclePositions;
 }
 
 async function main() {
   const grid = await readFile("input.txt", "utf-8").then((dataset) => {
     return dataset.split("\n").filter((line) => line.trim(""));
   });
+
   let part1Solution = solvePart1(new Simulator(grid));
-  console.log("Distinct Position:", part1Solution);
+  console.log("Part 1 Distinct Positions:", part1Solution);
+
+  console.log("Solving for part 2, may take a minute...");
+  let part2Solution = solvePart2(new Simulator(grid));
+  console.log("Part 2 Loop Obstacle Position:", part2Solution);
 }
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
--- 2024/Day 6/tests/solution.test.js
+++ 2024/Day 6/tests/solution.test.js
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "vitest";
-import { Guard, Simulator, solvePart1 } from "../index.js";
+import { Guard, Simulator, solvePart1, solvePart2 } from "../index.js";
 
 describe("Advent of Code 2024 Day 6", () => {
   test("Advent of Code provided test case #1", () => {
@@ -98,5 +98,76 @@ describe("Advent of Code 2024 Day 6", () => {
       simulator.guard.row === simulator.grid.length - 1 ||
         simulator.guard.column === simulator.grid[0].length - 1,
     ).toBe(true);
+  });
+
+  test("Insert O into simulator grid", () => {
+    let simulator = new Simulator([
+      "....#.....",
+      ".........#",
+      "..........",
+      "..#.......",
+      ".......#..",
+      "..........",
+      ".#..^.....",
+      "........#.",
+      "#.........",
+      "......#...",
+    ]);
+    let thing = simulator.grid[3];
+    thing = "..#O......";
+    simulator.grid[3] = thing;
+    let result = simulator.grid[3][3] === "O";
+    expect(result).toBe(true);
+  });
+
+  test("Solution run() Infinite Loop Detection", () => {
+    const grid = [
+      "....#.....",
+      ".........#",
+      "..........",
+      "..#.......",
+      ".......#..",
+      "..........",
+      ".#..^.....",
+      "........#.",
+      "#.........",
+      "......#...",
+    ];
+
+    const simulator = new Simulator(grid);
+
+    let result1 = true;
+    let result2 = true;
+    let originalRow1 = ".#..^.....";
+    let newRow1 = ".#.O^.....";
+    let newRow2 = "......#O..";
+
+    simulator.grid[6] = newRow1;
+    result1 = simulator.run();
+    expect(result1).toBe(false);
+
+    simulator.grid[6] = originalRow1;
+    simulator.grid[9] = newRow2;
+
+    result2 = simulator.run();
+    expect(result2).toBe(false);
+  });
+
+  test("Advent of Code provided test case #2", () => {
+    const grid = [
+      "....#.....",
+      ".........#",
+      "..........",
+      "..#.......",
+      ".......#..",
+      "..........",
+      ".#..^.....",
+      "........#.",
+      "#.........",
+      "......#...",
+    ];
+    const simulator = new Simulator(grid);
+    let result = solvePart2(simulator);
+    expect(result).toBe(6);
   });
 });
