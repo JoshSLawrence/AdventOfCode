c3dfee5b6c42aa65237967007078bf3048f3c6cc
Josh Lawrence <josh@joshlawrence.dev>
2025-12-03 06:52:53 -0500

    feat(2025): completed day 3 part 2

--- 2025/Day 3/main.go
+++ 2025/Day 3/main.go
@@ -8,19 +8,6 @@ import (
 	"strconv"
 )
 
-type Position int
-
-const (
-	Left Position = iota
-	Right
-)
-
-type Joltage struct {
-	index    int
-	value    int
-	position Position
-}
-
 func GetInput(filename string) []string {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -40,61 +27,33 @@ func GetInput(filename string) []string {
 	return input
 }
 
-func GetMaxJoltage(input string) int {
-	max1 := Joltage{
-		index:    -1,
-		value:    -1,
-		position: Left,
-	}
-	max2 := Joltage{
-		index:    -1,
-		value:    -1,
-		position: Left,
-	}
-	for i, ch := range input {
-		n, _ := strconv.Atoi(string(ch))
-		if n > max1.value {
-			max1.index = i
-			max1.value = n
+func GetMaxJoltage(input string, joltageLength int) int {
+	maxJoltage := ""
+	index := -1
+	for len(maxJoltage) < joltageLength {
+		maxV := -1
+		for i := (index + 1); i <= len(input)-(joltageLength-len(maxJoltage)); i++ {
+			v, _ := strconv.Atoi(string(input[i]))
+			if v > maxV {
+				maxV = v
+				index = i
+			}
 		}
+		s := strconv.Itoa(maxV)
+		maxJoltage += s
 	}
-
-	var slice string
-	if max1.index == len(input)-1 {
-		slice = input[:max1.index]
-		max1.position = Right
-		max2.position = Left
-	} else {
-		slice = input[(max1.index + 1):]
-		max1.position = Left
-		max2.position = Right
-	}
-
-	for i, ch := range slice {
-		n, _ := strconv.Atoi(string(ch))
-		if n > max2.value {
-			max2.index = i
-			max2.value = n
-		}
-	}
-
-	var temp string
-
-	if max1.position == Left {
-		temp = fmt.Sprintf("%d%d", max1.value, max2.value)
-	} else {
-		temp = fmt.Sprintf("%d%d", max2.value, max1.value)
-	}
-	maxJotlage, _ := strconv.Atoi(temp)
-
-	return maxJotlage
+	s, _ := strconv.Atoi(maxJoltage)
+	return s
 }
 
 func main() {
 	solution1 := 0
+	solution2 := 0
 	input := GetInput("input.txt")
 	for i := range input {
-		solution1 += GetMaxJoltage(input[i])
+		solution1 += GetMaxJoltage(input[i], 2)
+		solution2 += GetMaxJoltage(input[i], 12)
 	}
 	fmt.Println(solution1)
+	fmt.Println(solution2)
 }
--- 2025/Day 3/main_test.go
+++ 2025/Day 3/main_test.go
@@ -14,7 +14,7 @@ var testInput = []string{
 func TestPart1Cases(t *testing.T) {
 	for i := range testInput {
 		expected := 0
-		result := GetMaxJoltage(testInput[i])
+		result := GetMaxJoltage(testInput[i], 2)
 		switch i {
 		case 0:
 			expected = 98
@@ -28,5 +28,47 @@ func TestPart1Cases(t *testing.T) {
 		if result != expected {
 			t.Errorf("Expected: %v | Received: %v", expected, result)
 		}
+	}
+}
+
+func TestPart2Cases(t *testing.T) {
+	for i := range testInput {
+		expected := 0
+		result := GetMaxJoltage(testInput[i], 12)
+		switch i {
+		case 0:
+			expected = 987654321111
+		case 1:
+			expected = 811111111119
+		case 2:
+			expected = 434234234278
+		case 3:
+			expected = 888911112111
+		}
+		if result != expected {
+			t.Errorf("Expected: %v | Received: %v", expected, result)
+		}
+	}
+}
+
+func SolvePart1Example(t *testing.T) {
+	expected := 357
+	result := 0
+	for i := range testInput {
+		result += GetMaxJoltage(testInput[i], 2)
+	}
+	if result != expected {
+		t.Errorf("Expected: %v | Received: %v", expected, result)
+	}
+}
+
+func SolvePart2Example(t *testing.T) {
+	expected := 3121910778619
+	result := 0
+	for i := range testInput {
+		result += GetMaxJoltage(testInput[i], 12)
+	}
+	if result != expected {
+		t.Errorf("Expected: %v | Received: %v", expected, result)
 	}
 }
