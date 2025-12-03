5d5d41a05a35a83a446c8ce67533a585cbed24f3
Josh Lawrence <josh@joshlawrence.dev>
2025-12-03 00:05:39 -0500

    feat(2025): completed day 2 part 2

--- 2025/Day 2/main.go
+++ 2025/Day 2/main.go
@@ -9,6 +9,13 @@ import (
 	"strings"
 )
 
+type Constraint int
+
+const (
+	Part1 Constraint = iota
+	Part2
+)
+
 func getInput(filename string) []string {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -28,32 +35,63 @@ func getInput(filename string) []string {
 	return strings.Split(input[0], ",")
 }
 
-func isInvalid(n int) bool {
-	s := strconv.Itoa(n)
-	if len(s)%2 != 0 {
-		return false
-	}
-	mid := len(s) / 2
-	return s[:mid] == s[mid:]
-}
-
-func checkIdSequence(start int, end int) int {
-	sum := 0
+func GetInvalidIDs(start int, end int, constraint Constraint) []int {
+	invalidIds := []int{}
 	for i := start; i <= end; i++ {
-		if isInvalid(i) {
-			sum += i
+		s := strconv.Itoa(i)
+		n := len(s)
+		switch constraint {
+		case Part1:
+			if len(s)%2 != 0 {
+				continue
+			}
+			mid := len(s) / 2
+			if s[:mid] == s[mid:] {
+				invalidIds = append(invalidIds, i)
+			}
+		case Part2:
+			for l := 1; l <= n/2; l++ {
+				if n%l != 0 {
+					continue
+				}
+				part := s[:l]
+				repeated := strings.Repeat(part, n/l)
+				if repeated == s && n/l >= 2 {
+					invalidIds = append(invalidIds, i)
+					break
+				}
+			}
 		}
 	}
-	return sum
+	return invalidIds
 }
 
 func SolvePart1(testInput []string) int {
-	sum := 0
+	var invalidIds []int
 	for i := range testInput {
 		parts := strings.Split(testInput[i], "-")
 		start, _ := strconv.Atoi(parts[0])
 		end, _ := strconv.Atoi(parts[1])
-		sum += checkIdSequence(start, end)
+		invalidIds = append(invalidIds, GetInvalidIDs(start, end, Part1)...)
+	}
+	sum := 0
+	for _, v := range invalidIds {
+		sum += v
+	}
+	return sum
+}
+
+func SolvePart2(testInput []string) int {
+	var invalidIds []int
+	for i := range testInput {
+		parts := strings.Split(testInput[i], "-")
+		start, _ := strconv.Atoi(parts[0])
+		end, _ := strconv.Atoi(parts[1])
+		invalidIds = append(invalidIds, GetInvalidIDs(start, end, Part2)...)
+	}
+	sum := 0
+	for _, v := range invalidIds {
+		sum += v
 	}
 	return sum
 }
@@ -61,6 +99,7 @@ func SolvePart1(testInput []string) int {
 func main() {
 	input := getInput("input.txt")
 	solution1 := SolvePart1(input)
-
+	solution2 := SolvePart2(input)
 	fmt.Println(solution1)
+	fmt.Println(solution2)
 }
